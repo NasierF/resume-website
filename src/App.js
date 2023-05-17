53d08b500c1eea312f441ba1762d77299daf145b
NasierF <119966231+NasierF@users.noreply.github.com>
2023-05-17 18:48:55 -0400

    first real commit, website currently consists of a bacground image and some text explaining next steps.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App(){
   return (
     <div className="App" style = {appStyle}>
       <h1>Welcome to my website :)</h1>
-      <p>the first thing to begin working on is getting a top bar with working buttons</p>
+      <p>the first thing to begin working on is getting a top bar with working buttons.</p>
 
     </div>
   );
