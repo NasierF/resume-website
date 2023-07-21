27d577e6bb8d8ec29d67c7eb733cc88bd2c5a906
NasierF <119966231+NasierF@users.noreply.github.com>
2023-07-20 20:59:08 -0400

    actual resume added, having some trouble with making a css file to format the resume.js file

--- src/Resume.js
+++ src/Resume.js
@@ -1,16 +1,23 @@
 import React from 'react';
-//import App from './App';
-//import TopBar from "./TopBar";
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+//Resume.css iis currently not function, this is where to pick back up
+import './Resume.css';
+import Resumeimg from './NF_Resume.png';
 
 
 const Resume = () => {
     return( 
-    <div classname="Resume">
+    <div classname="resume-container">
         <h1> This is the page implemented for diplaying resume and achievments </h1>
         <p>here is where resume will be displayed</p>
+        <img src={Resumeimg} 
+        alt="Resume" 
+        style={{
+            width: '600px',   // Set the width of the image
+            height: '500px', // Set the height of the image
+            margin: '100px',// Set the margin around the image
 
+          }} 
+        />
     </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//ReactDOM.render(<App />, document.getElementById('root'));
-//ReactDOM.render(<Resume />, document.getElemetById('root'));
+
  root.render(
   <React.StrictMode>
     <App />
