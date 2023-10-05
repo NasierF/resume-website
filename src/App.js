faaf32dab7a41d5c87c18b5759291a4877890efe
NasierF <119966231+NasierF@users.noreply.github.com>
2023-10-05 16:40:26 -0400

    almost done css for desktops, adding animations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React /* , { useEffect } */ from 'react';
+//import { motion, useAnimation } from 'framer-motion';
 import './App.css';
 import TopBar from './TopBar';
 import Testing from './Resumepage';
 import Contact from './Contact';
 import Projects from './projectsPage';
-import slide1 from './TransRoboOwl.svg'
+//import slide1 from './TRO_fixed.svg'
 import github from './github.svg'
 import linkedin from './linkedin.svg'
+import mail from './envelope-solid.svg'
+import phone from './phone-solid.svg'
+import R from './react-2.svg'
+import java from './java-4.svg'
+import js from './javascript-1.svg'
+import sqlite from './sqlite.svg'
+import C from './c-1.svg'
+import ROS from './Ros_logo.svg.png'
+import python from './python-5.svg'
+import HTML from './html-1.svg'
+import Css from './css-3.svg'
+import dwn from './download-solid.svg'
+import LaTeXRes from './LaTeX_resume.pdf'
+//import QW from './QuizWizLogo.png'
 //add a projects page. -- add button to TopBAr for navigation
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import Button from './Button';
 
 function App(){
   return (
@@ -24,34 +40,70 @@ function App(){
   );
 }
 
-  const HomePage = () => {
+const HomePage = () => {
+      /* framer motion logic goes here, actual visual elements are retuned */
+    
+
+
+
+
+
+
     return(
     <div className="App">
       <div className='TitleConainer'>
         <h1>Hi!</h1>
-        <h2>I am <span>Nasier Fowlkes</span></h2>
-        <h3><span>Computer Science</span> senior at <span>Temple University</span></h3>
-        <h5> 
+        <h2>I am <span>Nasier Fowlkes</span>,</h2>
+        <h3><span>Computer Science</span> senior at <span>Temple University</span>.</h3>
+        <h5>
           <a href="https://github.com/NasierF" target="_blank" rel="noreferrer">
           <img src={github} alt='Github'/>
           </a>
           <a href='https://www.linkedin.com/in/nasier-fowlkes-09115b238/' target='_blank' rel="noreferrer">
             <img src={linkedin} alt='Linkedin'/>
           </a>
+          <a href='mailto:nasierfowlkes@yahoo.com' target='_blank' rel="noreferrer">
+            <img src={mail} alt='email'/>
+          </a>
+          <a href='tel:+6106627443' target='_blank' rel="noreferrer">
+            <img src={phone} alt='phone#'/>
+          </a>
+          <div className='resume-download'>
+            <a href={LaTeXRes} download="nasier-fowlkes-resume.pdf">
+              <p>download resume</p>
+              <img src={dwn} alt='resume download'/>
+            </a>
+          </div>
         </h5>
       </div>
       <div className='AboutMe'>
-       <h1> A little about myself: </h1>
-        <h4>
-          <p>I am the current <span>Web Manager</span> of Temple Robotics</p>
-          <img src={slide1} alt=''/>
-        </h4>
+        <h1> A little about myself: </h1>
+        <div className='bubbles'>
+            <p>I am the current <span>Web Manager</span> of Temple Robotics</p>
+        </div>
+        <div className='bubbles'>
+            <p>I participated in Temple Universitys <span>2023 Owlhacks</span> competition </p>
+        </div>
+        <div className='bubbles'>
+            <p>My current occupation is <span>IT Support</span> at Temple University</p>
+        </div>
+        <h1>My <span>Skills</span></h1>
+        <div className='line'><p></p></div>
+        <div className='image-container'>
+          <img src={R} alt='React'/>
+          <img src={java} alt='Java'/>
+          <img src={js} alt='JavaScript'/>
+          <img src={sqlite} alt='SQLite'/>
+          <img src={C} alt='C'/>
+          <img src={ROS} alt='ROS'/>
+          <img src={python} alt='Python'/>
+          <img src={HTML} alt='Python'/>
+          <img src={Css} alt='Css'/>
+        </div>
       </div>
-        
-      
     </div>
     );
-  }
+}
 
 
 export default App;
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,51 +1,45 @@
 import React from 'react';
 import Button from './Button';
 //import Dropdown from './Dropdown'
+import './TopBar.css'
 import { Link } from 'react-router-dom';
-import Tlogo from './Tlogo.png';
-//import TRlogo from './TransRoboOwl.svg'
-
+//import Tlogo from './Tlogo.png';
+//import bars from './bars-solid.svg';
 const TopBar = () => {
   const topBarStyle = {
     backgroundColor: '',
-    height: '50px',
+    height: '10vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'left',
     color: 'white',
     // Add any additional styles you want for the top bar
   };
+
+  
 // look into how to customize links like buttons
 
   return <div style={topBarStyle}>
-    <img src={Tlogo}
-        alt="Templelogo" 
-        style={{
-            width: '50px',   // Set the width of the image
-            height: '50px', // Set the height of the image
-            margin: '0px',// Set the margin around the image
+    <div className='TopBar-container'>
+      <h1>Nasier Fowlkes</h1>
 
-          }} 
-        />
-
-  <Link to = "/">
-      <Button label="Home" />
-    </Link>
-
-
-    <Link to = "/Resumepage">
-      <Button label ="resume"/>
-    </Link>
-
-    <Link to = "/Contact">
-      <Button label = "conact me"/>
-    </Link>
-
-    <Link to ='/ProjectsPage'>
-      <Button label ='Projects'/>
+      <Link to = "/">
+        <Button label="Home" />
       </Link>
-    
-    </div>;
+
+      <Link to = "/Resumepage">
+        <Button label ="resume"/>
+      </Link>
+
+      <Link to = "/Contact">
+        <Button label = "conact me"/>
+      </Link>
+
+      <Link to ='/ProjectsPage'>
+        <Button label ='Projects'/>
+      </Link>
+    </div>
+  </div>;
 };
 
 export default TopBar;
